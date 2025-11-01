69f03e20c89665844f0c7cb6b45e41014ba4b2a8
Prusotam <prusotamkumaryadav78@gmail.com>
2025-11-01 04:01:07 +0100

    little bit issue with native reusable fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import '../global.css'
 import { PortalHost } from '@rn-primitives/portal'
 export default function RootLayout() {
   return (
-  <>
-  <Stack/> 
-  <PortalHost/>
-  </>
+    <SafeAreaProvider>
+      <Stack/>
+      <PortalHost/>
+    </SafeAreaProvider>
   );
 }
